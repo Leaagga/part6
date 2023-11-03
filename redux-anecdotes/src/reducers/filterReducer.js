c458969698fa15ac72c33612380c43a63851633b
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-11-04 01:05:41 +0800

    6.10

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,16 +1,15 @@
-const filterReducer =(state='ALL',action)=>{
-  switch(action.type){
-    case "SET_FILTER":
-      return action.payload
-    default:
-      return state
+import { createSlice } from "@reduxjs/toolkit"
+
+const filterSlice =createSlice({
+  name:'filter',
+  initialState:'ALL',
+  reducers:{
+    filterChange(state,action){
+      const filter=action.payload
+      return filter
+    }
   }
-}
-export const filterChange=filter=>{
-  console.log(filter)
-  return {
-    type:"SET_FILTER",
-    payload:filter
-  }
-}
-export default filterReducer
+})
+
+export const { filterChange }=filterSlice.actions
+export default filterSlice.reducer
