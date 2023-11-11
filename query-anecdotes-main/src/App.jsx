77a886a2b93c1f2e9b5a95b2bfa7f899522ee172
Leaagga <135800909+Leaagga@users.noreply.github.com>
2023-11-11 17:26:09 +0800

    6.21

--- query-anecdotes-main/src/App.jsx
+++ query-anecdotes-main/src/App.jsx
@@ -1,9 +1,12 @@
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
-import { getAnecdotes } from './requests'
+import {  getAnecdotes } from './requests'
 const App = () => {
+
+
   const result = useQuery({
     queryKey: ['anecdotes'],
     queryFn: getAnecdotes,
--- query-anecdotes-main/src/components/AnecdoteForm.jsx
+++ query-anecdotes-main/src/components/AnecdoteForm.jsx
@@ -1,12 +1,30 @@
-const AnecdoteForm = () => {
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { createAnecdote } from "../requests"
 
+const AnecdoteForm = () => {
+  const queryClient=useQueryClient()
+  const newAnecdoteMutation = useMutation(
+    { mutationFn: createAnecdote,
+      onSuccess: ()=>{
+        queryClient.invalidateQueries({ queryKey: ['anecdotes'] })}
+      })
+    
+  const getId = () => (100000 * Math.random()).toFixed(0)
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     console.log('new anecdote')
-}
-
+    console.log(content)
+    if (content.length >=5){
+      newAnecdoteMutation.mutate({
+        content,
+        id:getId(),
+        votes:0
+      })
+    }else{
+      console.log('anecdote\'s length shorter than 5')
+    }}
   return (
     <div>
       <h3>create new</h3>
